31f20f4b02b5a45828423df11531b04c924eb911
mihajlo <mihajlo.rac@gmail.com>
2017-05-24 16:35:12 +0200

    final commit

--- app/src/main/java/com/example/mihajlo/shoppapp/activity/MainActivity.java
+++ app/src/main/java/com/example/mihajlo/shoppapp/activity/MainActivity.java
@@ -1,17 +1,12 @@
 package com.example.mihajlo.shoppapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.GridView;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.mihajlo.shoppapp.R;
 import com.example.mihajlo.shoppapp.adapter.ItemListAdapter;
-import com.example.mihajlo.shoppapp.model.Item;
 import com.example.mihajlo.shoppapp.repository.ItemDaoRepository;
 import com.example.mihajlo.shoppapp.utils.LoggedInUser;
 
@@ -20,13 +15,13 @@ import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.OnActivityResult;
+import org.androidannotations.annotations.OptionsItem;
+import org.androidannotations.annotations.OptionsMenu;
+import org.androidannotations.annotations.OptionsMenuItem;
 import org.androidannotations.annotations.ViewById;
 
-import java.sql.Array;
-import java.util.ArrayList;
-import java.util.Date;
-
 @EActivity(R.layout.activity_main)
+@OptionsMenu(R.menu.menu)
 public class MainActivity extends AppCompatActivity {
 
     @ViewById
@@ -35,17 +30,20 @@ public class MainActivity extends AppCompatActivity {
     @Bean
     ItemListAdapter adapter;
 
+    @OptionsMenuItem(R.id.logut)
+    MenuItem logout;
+
     @Bean
     ItemDaoRepository repository;
 
-    @AfterViews
-    void bindAdapter(){
-        updateList();
-        listview.setNumColumns(2);
-
+    @OptionsItem(R.id.logut)
+    void logout() {
+        LoggedInUser.getInstance().setUser(null);
+        LoginActivity_.intent(this).start();
     }
 
-    private void setNumCols(){
+    @OptionsItem(R.id.order)
+    void setOrder() {
         if(listview.getNumColumns() == 1){
             listview.setNumColumns(2);
         }else{
@@ -53,35 +51,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return true;
+    @OptionsItem(R.id.myAccount)
+    void myAccount(){
+        if(LoggedInUser.getInstance().getUser() !=null){
+            MyAccount_.intent(this).startForResult(0);
+        }else{
+            Toast.makeText(this,"Guest don't have items lists",Toast.LENGTH_LONG).show();
+        }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        switch (item.getItemId()) {
-            case R.id.logut:
-                LoggedInUser.getInstance().setUser(null);
-                LoginActivity_.intent(this).start();
-                return true;
-            case R.id.myAccount:
-                if(LoggedInUser.getInstance().getUser() !=null){
-                    MyAccount_.intent(this).startForResult(0);
-                }else{
-                    Toast.makeText(this,"Please login to view your account",Toast.LENGTH_LONG).show();
-                }
-
-                return true;
-            case R.id.order:
-                setNumCols();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+    @AfterViews
+    void bindAdapter(){
+        updateList();
+        listview.setNumColumns(2);
     }
 
     @Override
